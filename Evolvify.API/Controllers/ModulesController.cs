7ed2a769698a8d8effcdce55be18fd6ae3a13ff7
Ahmed-Bashar <ahmedbashar01021085124@gmail.com>
2025-02-20 02:01:48 +0200

    ModuleController

---
+++ Evolvify.API/Controllers/ModulesController.cs
@@ -0,0 +1,69 @@
+﻿using Evolvify.Application.Modules.Command.CreateModule;
+using Evolvify.Application.Modules.Command.DeleteModule;
+using Evolvify.Application.Modules.Command.UpdateModule;
+using Evolvify.Application.Modules.Queries.GetAll;
+using Evolvify.Application.Modules.Queries.GetById;
+using Evolvify.Application.Skills.Commands.CreateSkill;
+using Evolvify.Application.Skills.Commands.DeleteSkill;
+using Evolvify.Application.Skills.Commands.UpdateSkill;
+using Evolvify.Application.Skills.Queries.GetById;
+using Evolvify.Application.Skills.Query.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Evolvify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModulesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ModulesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetModule()
+        {
+            var response = await mediator.Send(new GetAllModuleQuery());
+
+            return response.Success ? Ok(response) : NotFound(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetModule([FromRoute] int id)
+        {
+            var response = await mediator.Send(new GetModulrByIdQuery(id));
+
+            return response.Success ? Ok(response) : NotFound(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateModule([FromBody] CreateModuleCommand command)
+        {
+            var response = await mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetModule), new { id = response.Data.Id }, null);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateModule([FromRoute] int id, [FromBody] UpdateModuleCommand command)
+        {
+            command.Id = id;
+            await mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModule([FromRoute] int id)
+        {
+            await mediator.Send(new DeleteModuleCommand(id));
+
+            return NoContent();
+        }
+
+    }
+}
