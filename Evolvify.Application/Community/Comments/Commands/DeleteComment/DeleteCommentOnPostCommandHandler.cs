0c4fc63a63af4deeb3db7367d504b9f2d3b68289
hishamabdalla <hishvmabdalla@gmail.com>
2025-03-09 23:49:56 +0200

    Delete Comment Handler

---
+++ Evolvify.Application/Community/Comments/Commands/DeleteComment/DeleteCommentOnPostCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Evolvify.Domain.Entities.Community;
+using Evolvify.Domain.Exceptions;
+using Evolvify.Infrastructure.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolvify.Application.Community.Comments.Commands.DeleteComment
+{
+    public class DeleteCommentOnPostCommandHandler : IRequestHandler<DeleteCommentOnPostCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public async Task Handle(DeleteCommentOnPostCommand request, CancellationToken cancellationToken)
+        {
+            var post = await _unitOfWork.Repository<Post,Guid>().GetByIdAsync(request.PostId);
+            
+            if (post == null)
+            {
+                throw new NotFoundException(nameof(Post), request.PostId.ToString());
+            }
+
+            var comment = post.Comments.FirstOrDefault(x => x.Id == request.CommentId);
+            if (comment == null)
+            {
+                throw new NotFoundException(nameof(Comment), request.CommentId.ToString());
+            }
+            else
+            {
+                _unitOfWork.Repository<Comment, Guid>().Delete(comment);
+                await _unitOfWork.CompleteAsync();
+            }
+
+
+        }
+    }
+}
