90848e187233cdae4f33e2b1bb568e3a92fe29ff
hishamabdalla <hishvmabdalla@gmail.com>
2025-03-10 00:43:01 +0200

    Get All Comment For Post

---
+++ Evolvify.Application/Community/Comments/Queries/GetAllCommentForPost/GetAllCommentForPostQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Evolvify.Application.Community.Comments.DTOs;
+using Evolvify.Application.DTOs.Response;
+using Evolvify.Domain.Entities.Community;
+using Evolvify.Domain.Exceptions;
+using Evolvify.Infrastructure.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolvify.Application.Community.Comments.Queries.GetAllCommentForPost
+{
+    public class GetAllCommentForPostQueryHandler : IRequestHandler<GetAllCommentForPostQuery, ApiResponse<IEnumerable<CommentDto>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public GetAllCommentForPostQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+
+            
+        }
+
+        public async Task<ApiResponse<IEnumerable<CommentDto>>> Handle(GetAllCommentForPostQuery request, CancellationToken cancellationToken)
+        {
+            var post = await _unitOfWork.Repository<Post, Guid>().GetByIdAsync(request.PostId);
+            if (post == null)
+            {
+                throw new NotFoundException(nameof(Post),request.PostId.ToString());
+            }
+               
+            if(post.Comments.Any())
+            {
+                var comments=_mapper.Map<IEnumerable<CommentDto>>(post.Comments);
+                return new ApiResponse<IEnumerable<CommentDto>>(comments);
+            }
+
+            throw new NotFoundException("Comments not found");
+
+
+            
+        }
+    }
+}
