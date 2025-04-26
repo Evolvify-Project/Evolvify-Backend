b6118d8e4355d559c384c6c944b5618462c16c77
Ahmed-Bashar <ahmedbashar01021085124@gmail.com>
2025-04-26 07:42:58 +0300

    profilepicture

---
+++ Evolvify.API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Evolvify.Application.Common.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Evolvify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost] 
+        public async Task<IActionResult> UploadPicture(ProfilePicture profilePicture )
+        {
+            using var stream = new MemoryStream();
+            await profilePicture.Image.CopyToAsync(stream);
+            var image = new Image 
+            {
+                Id = profilePicture.Id
+                ,image =  profilePicture
+            };
+
+            return Ok(image);
+        }   
+    }
+}
