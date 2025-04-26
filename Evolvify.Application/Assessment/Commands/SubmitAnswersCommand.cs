c0854e554fbe4628182881c63c737552d9b79269
hishamabdalla <hishvmabdalla@gmail.com>
2025-04-26 21:50:18 +0300

    "feat: added SubmitAnswersCommandHandler"

---
+++ Evolvify.Application/Assessment/Commands/SubmitAnswersCommand.cs
@@ -0,0 +1,21 @@
+﻿using Evolvify.Application.DTOs.Response;
+using Evolvify.Domain.Entities.Assessment;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolvify.Application.Assessment.Commands
+{
+    public class SubmitAnswersCommand : IRequest<ApiResponse<PredictionResponse>>
+    {
+        public SkillAnswer SkillAnswer { get; set; }
+        public SubmitAnswersCommand(SkillAnswer skillAnswer)
+        {
+            SkillAnswer = skillAnswer;
+        }
+
+    }
+}
