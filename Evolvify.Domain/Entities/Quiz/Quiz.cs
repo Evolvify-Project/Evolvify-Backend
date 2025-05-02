121f6d75cb03dea1a8e3e034dcaa863b0fa98798
ansumesse <manas8269@gmail.com>
2025-05-02 08:10:22 +0300

    Add Duration to Quiz

--- Evolvify.Domain/Entities/Quiz/Quiz.cs
+++ Evolvify.Domain/Entities/Quiz/Quiz.cs
@@ -10,6 +10,7 @@ namespace Evolvify.Domain.Entities.Quiz
     {
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;      
+        public string Duration { get; set; } = string.Empty;      
         public List<Question> Questions { get; set; } = new List<Question>();
     }
 
---
+++ Evolvify.Infrastructure/Migrations/20250430181025_AddDurationToQuiz.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evolvify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDurationToQuiz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Duration",
+                table: "Quizs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Quizs");
+        }
+    }
+}
