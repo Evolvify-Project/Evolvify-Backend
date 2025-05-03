9c6aac20906d9c1ed76f2618108f134915ffcd0b
Hishvm <129236724+hishamabdalla@users.noreply.github.com>
2025-05-03 12:19:28 +0300

    Revert "Besh"

--- Evolvify.Domain/Entities/ApplicationUser.cs
+++ Evolvify.Domain/Entities/ApplicationUser.cs
@@ -11,7 +11,7 @@ namespace Evolvify.Domain.Entities
 {
     public class ApplicationUser:IdentityUser
     {
-        public string ProfileImage { get; set; } = string.Empty;
-
+       
+      
     }
 }
