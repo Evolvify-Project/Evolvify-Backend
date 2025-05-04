7331580b097fab5f2b8d79491ff89456c3315cab
hishamabdalla <hishvmabdalla@gmail.com>
2025-05-04 04:22:51 +0300

    feat: update user profile image

---
+++ Evolvify.Application/Identity/UserProfile/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolvify.Application.Identity.UserProfile.Commands.UpdateUser
+{
+    public class UpdateUserCommand: IRequest
+    {
+        public IFormFile? Image { get; set; }
+    }
+    
+}
---
+++ Evolvify.Application/Identity/UserProfile/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using Evolvify.Application.Common.User;
+using Evolvify.Domain.Entities;
+using Evolvify.Domain.Interfaces.ImageInterface;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolvify.Application.Identity.UserProfile.Commands.UpdateUser
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand>
+    {
+        private readonly IFileService _fileService;
+        private readonly IUserContext _userContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UpdateUserCommandHandler(IFileService fileService, IUserContext userContext, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _fileService = fileService;
+            _userContext = userContext;
+        }
+        public async Task Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var UserId = _userContext.GetCurrentUser().Id;
+
+            var user = await _userManager.FindByIdAsync(UserId);
+
+            if (request.Image != null)
+            {
+                var imageUrl = user.ProfileImageUrl;
+
+                if (!string.IsNullOrEmpty(imageUrl))
+                {
+                    _fileService.DeleteImage(imageUrl);
+                }
+                var imagePath = await _fileService.UploadImage(request.Image);
+                user.ProfileImageUrl = imagePath;
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Failed to update user profile image");
+                }
+
+
+
+            }
+        }
+
+    }
+}
